87baa0107d31e9a7d88c9a823fc43da9b8488264
Nathan <frevnjengamungai@gmail.com>
2022-10-19 02:40:43 -0700

    printf functions

--- printf_functions.c
+++ printf_functions.c
@@ -1,25 +1,31 @@
-#include <>
+#include "main.h"
 /**
  * print_char - prints a character
  * @a: character printed
  * Returns nothing
  */
-void print_char(char a)
+void print_char(va_list c)
 {
+	char a = (char)va_arg(c, int);
 	_putchar(a);
 }
 
 /**
  * print_string - prints a string
- * @a: string printed
+ * @str: string printed
  * Return: nothing
  */
-void print_string(char *a)
+void print_string(va_list s)
 {
-	int n = strlen(a);
+	char *str = va_arg(s, char *);
+	
+	int n = strlen(str);
+
+	if (str == NULL)
+		str = "(null)";
 	for(int i = 0; i < n; i++)
 	{
-		_putchar(a[i]);
+		_putchar(str[i]);
 	}
 }
 
@@ -28,10 +34,23 @@ void print_string(char *a)
  * @a: integer printed
  * Return: nothing
  */
-void print_int(int a)
+void print_int(va_list i)
 {
-	int d, counter;
-	d = a;
+	int a = va_arg(i, int);
+	unsigned int num, d, counter;
+	
+	if(a < 0)
+	{
+		_putchar(45);
+		num =- a;
+
+	}
+	else
+	{
+		num = a;
+	}
+
+	d = num;
 	counter = 1;
 	while(d > 9)
 	{
@@ -40,6 +59,6 @@ void print_int(int a)
 	}
 	for (; counter >= 1; counter /= 10)
 	{
-		_putchar(((a / counter) % 10) + 48);
+		_putchar(((num / counter) % 10) + 48);
 	}
 }
